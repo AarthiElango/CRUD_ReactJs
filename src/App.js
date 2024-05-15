35ae95fb64a33a5bc8effe1c57cf2be6426b2153
AARTHI ELANGOVAN <aarthi7813@gmail.com>
2024-05-15 18:02:40 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import { Button } from 'bootstrap';
 import './App.css';
 import Create from './components/Create';
 import Read from './components/Read';
 import Update from './components/Update';
 import { img } from './Constants/URL';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   
   return (
     <div className="app-container">
       <h2>EMPLOYEE LIST</h2>
+     
       <a href='/create'>
       <img src={img} alt="My Logo" className="logo" />
       </a>
+    
       <BrowserRouter>
         <Routes>
+          
+          <Route exact path='/' element={<Create />}/>
           <Route exact path='/create' element={<Create />}/>
           <Route exact path='/read' element={<Read />}/>
           <Route exact path='/update' element={<Update/>}/>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> 
+      
     </div>
   );
 }
